604ee37beddf14b7ae50ccea8dd15c3e8bd6273a
Thomas Richard <thomas.richard@bootlin.com>
2023-12-14 17:08:44 +0100

    sciclient: s2r: no need to save BL31 resume address anymore
    Since BL31 detects if the board is resuming and restores its context,
    there is no need to save its resume address.
    
    Signed-off-by: Thomas Richard <thomas.richard@bootlin.com>

--- packages/ti/drv/sciclient/src/sciclient/sciclient_s2r.c
+++ packages/ti/drv/sciclient/src/sciclient/sciclient_s2r.c
@@ -60,19 +60,6 @@ static void asm_function(void);
 /* ========================================================================== */
 
 /**
- * This address belongs to the space reserved from the DM firmware for
- * the point of view of Linux, U-boot SPL must use the same address
- * to restore BL31 ATF and resume entry point address.
- */
-#define SCLICLIENT_ATF_BL31_DDR_RESTORE_ADDRESS (0xA5000000U)
-
-/**
- * This is actually the biggest possible size of BL31 ATF. This value is defined
- * in ATF.
- */
-#define SCICLIENT_ATF_BL31_CODE_SIZE (0x20000U)
-
-/**
  * Address in BT SRAM where to load the code that will take care of the
  * DDR retention and PMIC S2R configuration.
  */
@@ -106,12 +93,8 @@ static void S2R_cleanAllDCache(void)
     }
 }
 
-void S2R_goRetention(uint32_t coreResumeAddr)
+void S2R_goRetention(void)
 {
-    volatile uint32_t* ptr_address = (volatile uint32_t*)(SCLICLIENT_ATF_BL31_DDR_RESTORE_ADDRESS + SCICLIENT_ATF_BL31_CODE_SIZE);
-    CSL_REG32_WR(ptr_address,coreResumeAddr);
-    S2R_debugPrintf("Resume address stored = 0x%x\n", coreResumeAddr);
-
     S2R_cleanAllDCache();
 
     /* load DDR retention code and PMIC S2R into SRAM */
