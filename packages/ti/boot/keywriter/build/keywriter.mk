26b0586d6394445d804479d96e31615212ca14db
Trupthi <t-patil1@ti.com>
2024-11-16 17:54:02 +0530

    Revert "Revert keywriter PRs to fix random build failures"
    This reverts commit f5cec1f19d1a064d77e7813652866d14357d33bb.

--- packages/ti/boot/keywriter/build/keywriter.mk
+++ packages/ti/boot/keywriter/build/keywriter.mk
@@ -4,12 +4,17 @@
 #
 include $(PDK_INSTALL_PATH)/ti/build/Rules.make
 
-APP_NAME                     = keywriter_img
-LOCAL_APP_NAME               = keywriter_img_$(SOC)
+ifeq ($(SBL_IMAGE_TYPE), combined)
+  APP_NAME                     = keywriter_img_combined
+  LOCAL_APP_NAME               = keywriter_img_combined_$(SOC)
+else
+  APP_NAME                     = keywriter_img
+  LOCAL_APP_NAME               = keywriter_img_$(SOC)
+endif
 BUILD_OS_TYPE                = baremetal
 
 # J784S4 EVM does not use PMIC
-ifeq ($(BOARD),j784s4_evm)
+ifneq ($(filter $(BOARD),j784s4_evm j742s2_evm),)
 VPP_EN_CONTROL               = gpio
 else
 VPP_EN_CONTROL               = pmic
@@ -60,6 +65,12 @@ ifeq ($(SOC),j721e)
   CFLAGS_LOCAL_COMMON		+= -DJ721E_USE_GPIO_FOR_VPP
 endif
 
+# Combined boot image flags
+ifeq ($(SBL_IMAGE_TYPE), combined)
+  SBL_CFLAGS += -DSBL_COMBINED_BOOT
+  CFLAGS_LOCAL_COMMON += -DSBL_COMBINED_BOOT
+endif
+
 SRCS_COMMON                 += main.c
 ifeq ($(VPP_EN_CONTROL),pmic)
 SRCS_COMMON                 += soc/common/pmic_example.c
@@ -88,6 +99,7 @@ ifeq ($(MAKERULEDIR), )
   MAKERULEDIR               := $(ROOTDIR)/ti/build/makerules
   export MAKERULEDIR
 endif
+include $(MAKERULEDIR)/platform.mk
 include $(MAKERULEDIR)/common.mk
 include $(PDK_KEYWR_COMP_PATH)/build/bin2c.mk
 
