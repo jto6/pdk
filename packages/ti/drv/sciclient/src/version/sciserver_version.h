04604b21857c4b4b171a10f7f255c77f1469f88d
Vamshi Vemula <v-vemula@ti.com>
2025-01-28 14:13:18 +0530

    [Sciclient][PDK-16846] Sciserver memory map fix
    - SBL_USED_OCMC_RAM is the SBL memory reused by sciserver_testapp as
    .bss section
    - However this memory contains WFI code for mcu1_1 which causes memory
    map conflict.
    - This fails pm_baremetal_systemconfig_testapp example as it overwrites
    the WFI code leading to mcu1_1 executing garbage code.
    - Moved .bss start address in sciserver_testapp by 0x1000 for avoiding
    WFI code corruption.
    
    Fixes: PDK-16846
    
    Signed-off-by: Vamshi Vemula <v-vemula@ti.com>

--- packages/ti/drv/sciclient/src/version/sciserver_version.h
+++ packages/ti/drv/sciclient/src/version/sciserver_version.h
@@ -1,7 +1,7 @@
 /**
  * SCISERVER Version Info
  *
- * Copyright (C) 2024 Texas Instruments Incorporated - http://www.ti.com/
+ * Copyright (C) 2025 Texas Instruments Incorporated - http://www.ti.com/
  *
  * This software is licensed under the  standard terms and conditions in the
  * Texas Instruments  Incorporated Technology and Software Publicly Available
@@ -18,7 +18,7 @@
 #define SCISERVER_MAJOR_VERSION_NAME	"v09.02.00"
 #define SCISERVER_SUBVERSION	0
 #define SCISERVER_PATCHVERSION	0
-#define SCISERVER_SCMVERSION	"-REL.PSDK.10.01.00.18-1-g746f5+"
+#define SCISERVER_SCMVERSION	"-REL.PSDK.10.01.00.25-2-gdd7e2+"
 
 #endif /* INCLUDE_SCISERVER_VERSION_H */
 
