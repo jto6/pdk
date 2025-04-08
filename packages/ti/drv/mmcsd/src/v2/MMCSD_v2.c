a69b2799e2b26694f3a9e00eead338ff5eb711a4
Gokul Praveen <g-praveen@ti.com>
2025-04-08 14:52:02 +0530

    PDK-17330:[EMMC]:Fix typecasting issue
        The extCid double pointer integer variable points to the
        address of an integer variable.
    
        Assigning a double pointer to a single pointer casted to a
        double pointer causes overwriting of neighbouring variables.

--- packages/ti/drv/mmcsd/src/v2/MMCSD_v2.c
+++ packages/ti/drv/mmcsd/src/v2/MMCSD_v2.c
@@ -2071,7 +2071,8 @@ MMCSD_Error MMCSD_switch_eMMC_mode(MMCSD_Handle handle, MMCSD_SupportedMMCModes_
     MMCSD_v2_HwAttrs const *hwAttrs = NULL;
     MMCSD_v2_Transaction    transaction;
     uint32_t drvStrength_controller=0;
-    uint8_t  **extCid = (uint8_t**) &gExtCidAddr;
+    uint8_t *extcidsptr = (uint8_t*)&gExtCidAddr;
+    uint8_t  **extCid = (uint8_t**) &extcidsptr;
     uint8_t  driveStrengthSupportRegInExdCsd = 0;
     /* Get the pointer to the object and hwAttrs */
     object = (MMCSD_v2_Object *)((MMCSD_Config *) handle)->object;
