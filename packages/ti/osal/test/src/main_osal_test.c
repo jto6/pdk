3339dd9d94d41052b81f637f032f4b52a876386a
S Likhith <likhith@ti.com>
2025-08-08 17:18:25 +0530

    [OSAL]: Fix osal baremetal testapp data abort
    - Osal baremetal testapp goes to a data abort due to mis-aligned
    external memory block for semaphores and interrupts
    - Forcing an alignment to resolve the issue
    
    Signed-off-by: S Likhith <likhith@ti.com>

--- packages/ti/osal/test/src/main_osal_test.c
+++ packages/ti/osal/test/src/main_osal_test.c
@@ -1462,8 +1462,10 @@ bool OSAL_cache_test()
 #if defined (BARE_METAL)
   #define SEMP_BLOCK_SIZE (OSAL_TEST_NUM_EXT_SEMAPHORES * OSAL_NONOS_SEMAPHOREP_SIZE_BYTES)
   #define HWIP_BLOCK_SIZE (OSAL_TEST_NUM_EXT_HWIPS * OSAL_NONOS_HWIP_SIZE_BYTES)
-  uint8_t semPMemBlock[SEMP_BLOCK_SIZE];
-  uint8_t hwiPMemBlock[HWIP_BLOCK_SIZE];
+  uint8_t semPMemBlock[SEMP_BLOCK_SIZE]
+  __attribute__ ((aligned(64)));
+  uint8_t hwiPMemBlock[HWIP_BLOCK_SIZE]
+  __attribute__ ((aligned(64)));
 #elif defined (FREERTOS)
   #define SEMP_BLOCK_SIZE (OSAL_TEST_NUM_EXT_SEMAPHORES * OSAL_FREERTOS_SEMAPHOREP_SIZE_BYTES)
   #if defined (BUILD_C7X)
